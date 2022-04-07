7bde6b0155f5c161ed87c30a81c28d95e7ec793d
“suchapalaver” <jlivesey@gmail.com>
2022-04-07 16:44:43 -0400

    Add `BlockchainAddress`

---
+++ src/blockchain_address.rs
@@ -0,0 +1,18 @@
+//! src/blockchain_address.rs
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BlockchainAddress {
+    // page: i64,
+    // total_pages: i64,
+    // items_on_page: i64,
+    pub address: String,
+    // balance: String,
+    // total_received: String,
+    // total_sent: String,
+    // unconfirmed_balance: String,
+    // unconfirmed_txs: i64,
+    // txs: i64,
+    pub txids: Vec<String>
+}
+
