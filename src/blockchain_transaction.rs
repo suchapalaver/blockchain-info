5bb8ddc5e75057ac38c830c36c230ee21e42fab5
“suchapalaver” <jlivesey@gmail.com>
2022-04-07 16:49:28 -0400

    Add `BlockchainTransaction` `Vout` and `Vin`

---
+++ src/blockchain_transaction.rs
@@ -0,0 +1,42 @@
+//! src/blockchain_transaction.rs
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Vin {
+    // txid: String,
+    // vout: i64,
+    // sequence: i64,
+    // n: i64,
+    pub addresses: Vec<String>,
+    // is_address: bool,
+    pub value: String,
+    // hex: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Vout {
+    pub value: String,
+    // n: i64,
+    // hex: String,
+    pub addresses: Vec<String>,
+    // is_address: bool
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BlockchainTransaction {
+    pub txid: String,
+    // version: i64,
+    // lock_time: i64,
+    pub vin: Vec<Vin>,
+    pub vout: Vec<Vout>,
+    // block_hash: String,
+    // block_height: i64,
+    // confirmations: i64,
+    // block_time: i64,
+    // value: String,
+    // value_in: String,
+    // fees: String,
+    // hex: String,
+}
