7b66551185f964146df18df3827916cf3db14c00
“suchapalaver” <jlivesey@gmail.com>
2022-04-07 21:08:03 -0400

    Fix imports

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@
 #[macro_use]
 extern crate serde_derive;
 
-mod blockchain_address;
-mod blockchain_status;
-mod blockchain_transaction;
-mod blockchain_info;
+pub mod blockchain_address;
+pub mod blockchain_status;
+pub mod blockchain_transaction;
+pub mod blockchain_info;
 
-pub use crate::blockchain_address::*;
-pub use crate::blockchain_status::*;
-pub use crate::blockchain_transaction::*;
-pub use crate::blockchain_info::*;
+pub use blockchain_address::*;
+pub use blockchain_status::*;
+pub use blockchain_transaction::*;
+pub use blockchain_info::*;
