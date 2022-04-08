33abc529bc4e9315bccb476ff69cc4468046dc5f
“suchapalaver” <jlivesey@gmail.com>
2022-04-07 21:14:24 -0400

    Add `entered_address` value and `blockchain_address` request; move `blockchain_address` and `blockchain_status` to `blockchain_info_app`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 //! src/main.rs
 
-use blockchain_info::BlockchainStatus;
+use {
+    blockchain_info::blockchain_info::blockchain_address_request,
+    blockchain_info::blockchain_status::BlockchainStatus,
+    blockchain_info::blockchain_address::BlockchainAddress,
+    blockchain_info::blockchain_transaction::BlockchainTransaction,
+    dotenv,
+};
 
-fn main() {
+fn blockchain_info_app(address: &str) {
     let blockchain_status: BlockchainStatus = blockchain_info::blockchain_status_request();
     println!("\n\nQuerying {} - chain: {}\n\n", &blockchain_status.blockbook.coin, &blockchain_status.backend.chain);
+
+    let blockchain_address: BlockchainAddress = blockchain_info::blockchain_address_request(address);
+    println!("\n\nAnalyzing transactions for Bitcoin address {}", &blockchain_address.address);
+}
+
+fn main() {
+    let entered_address = dotenv::var("WALLET").expect("Error reading env var");
+    blockchain_info_app(&entered_address);
 }
