638e2b079b1b2d84d6aeb8540952a0bd9bac9daa
“suchapalaver” <jlivesey@gmail.com>
2022-04-07 20:26:52 -0400

    Add `send_request` `blockchain_status_request` in src/blockchain_info.rs and blockchain status request to `main`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//! src/main.rs
+
+use blockchain_info::BlockchainStatus;
+
 fn main() {
-    println!("Hello, world!");
+    let blockchain_status: BlockchainStatus = blockchain_info::blockchain_status_request();
+    println!("\n\nQuerying {} - chain: {}\n\n", &blockchain_status.blockbook.coin, &blockchain_status.backend.chain);
 }
